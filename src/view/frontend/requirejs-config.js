3d33e5a50c70fb0bab7dfab80297702ec6cdf886
Rodion Kachkin <r.kachkin@mygento.com>
2024-03-30 21:34:40 +0400

    Add paths to requirejs-config.js

---
+++ src/view/frontend/requirejs-config.js
@@ -0,0 +1,9 @@
+let config = {
+  paths: {
+    "json": 'Qunity_Downloadable/js/lib/requirejs/json.min',
+    "async": 'Qunity_Downloadable/js/lib/requirejs/async.min',
+    "videojs": "Qunity_Downloadable/js/lib/videojs/video.min",
+    "videojs/youtube": "Qunity_Downloadable/js/lib/videojs/youtube.min",
+    "videojs/lang/ru": "Qunity_Downloadable/js/lib/videojs/lang/ru.min.json"
+  }
+}
