df242ec4c9233f5ec59f3958c1bd0c3aea739fe7
Rodion Kachkin <r.kachkin@mygento.com>
2024-03-31 11:58:13 +0400

    Remove async plugin from requirejs

--- src/view/frontend/requirejs-config.js
+++ src/view/frontend/requirejs-config.js
@@ -1,7 +1,6 @@
 let config = {
   paths: {
     "json": 'Qunity_Downloadable/js/lib/requirejs/json.min',
-    "async": 'Qunity_Downloadable/js/lib/requirejs/async.min',
     "videojs": "Qunity_Downloadable/js/lib/videojs/video.min",
     "videojs/youtube": "Qunity_Downloadable/js/lib/videojs/youtube.min",
     "videojs/lang/ru": "Qunity_Downloadable/js/lib/videojs/lang/ru.min.json"
