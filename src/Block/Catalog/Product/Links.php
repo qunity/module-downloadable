0b9e1b827822ab0c41e03592c1feed60a874eaf0
Rodion Kachkin <r.kachkin@mygento.com>
2024-04-03 21:53:22 +0400

    Add links rewrite for downloadable product

---
+++ src/Block/Catalog/Product/Links.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Qunity\Downloadable\Block\Catalog\Product;
+
+use Magento\Downloadable\Api\Data\LinkExtension;
+use Magento\Downloadable\Api\Data\LinkInterface as MagentoLinkInterface;
+use Magento\Downloadable\Block\Catalog\Product\Links as BaseLinks;
+use Qunity\Downloadable\Api\Data\LinkInterface as QunityLinkInterface;
+use Qunity\Downloadable\Model\Data\Link;
+
+class Links extends BaseLinks
+{
+    /**
+     * Checking whether the link have online status
+     *
+     * @param MagentoLinkInterface $link
+     * @return bool
+     */
+    public function isOnlineLink(MagentoLinkInterface $link): bool
+    {
+        return (bool) $this->getQunityLink($link)?->getIsOnlineLink();
+    }
+
+    /**
+     * Checking whether the link sample have online status
+     *
+     * @param MagentoLinkInterface $link
+     * @return bool
+     */
+    public function isOnlineSample(MagentoLinkInterface $link): bool
+    {
+        return (bool) $this->getQunityLink($link)?->getIsOnlineSample();
+    }
+
+    /**
+     * Get extension Qunity attributes object
+     *
+     * @param MagentoLinkInterface $link
+     * @return QunityLinkInterface|null
+     */
+    private function getQunityLink(MagentoLinkInterface $link): ?QunityLinkInterface
+    {
+        /** @var LinkExtension|null $extension */
+        $extension = $link->getExtensionAttributes();
+        if (!$extension) {
+            return null;
+        }
+
+        /** @var Link|null $qunity */
+        $qunity = $extension->getQunity();
+        if (!$qunity) {
+            return null;
+        }
+
+        return $qunity;
+    }
+}
