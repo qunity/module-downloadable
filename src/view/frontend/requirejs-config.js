fbe1a8db945841721f8f268832eeb73373104747
Rodion Kachkin <r.kachkin@mygento.com>
2024-04-03 22:38:28 +0400

    Update requirejs-config.js for frontend

--- src/view/frontend/requirejs-config.js
+++ src/view/frontend/requirejs-config.js
@@ -1,4 +1,11 @@
 let config = {
+  map: {
+    "*": {
+      "video/player": "Qunity_Downloadable/js/video/player",
+      "video/component/title-bar": "Qunity_Downloadable/js/video/component/title-bar",
+      "video/component/big-button": "Qunity_Downloadable/js/video/component/big-button"
+    }
+  },
   paths: {
     "json": 'Qunity_Downloadable/js/lib/requirejs/json.min',
     "videojs": "Qunity_Downloadable/js/lib/videojs/video.min",
