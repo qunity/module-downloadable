0a770ff716198ca5e7d4455a18f4a7da0ebedd62
Rodion Kachkin <r.kachkin@mygento.com>
2024-06-03 22:01:00 +0400

    Release 0.1.8

--- src/Block/Catalog/Product/Links.php
+++ src/Block/Catalog/Product/Links.php
@@ -9,9 +9,35 @@ use Magento\Downloadable\Api\Data\LinkInterface as MagentoLinkInterface;
 use Magento\Downloadable\Block\Catalog\Product\Links as BaseLinks;
 use Qunity\Downloadable\Api\Data\LinkInterface as QunityLinkInterface;
 use Qunity\Downloadable\Model\Data\Link;
+use Qunity\Video\Api\Data\VideoPlayer\ConfigInterface;
+use Qunity\Video\Block\VideoPlayer;
 
 class Links extends BaseLinks
 {
+    /**
+     * Get child Video Player block and configure it by link info
+     *
+     * @param MagentoLinkInterface $link
+     * @return VideoPlayer|null
+     */
+    public function getVideoPlayerBlock(MagentoLinkInterface $link): ?VideoPlayer
+    {
+        /** @var VideoPlayer $videoPlayer */
+        $videoPlayer = $this->getChildBlock('player');
+        if (empty($videoPlayer)) { // @phpstan-ignore-line
+            return null;
+        }
+
+        $data = [
+            ConfigInterface::LINK_URL => $link->getSampleUrl(),
+            'product_id' => $this->getProduct()->getId(),
+            'link_id' => $link->getId(),
+            'title' => $link->getTitle(),
+        ];
+
+        return $videoPlayer->update($data);
+    }
+
     /**
      * Checking whether link have Online flag
      *
